5e9b49e562455fd479d07ac44936142f67323a3b
PartOfEarth <arnavlotlikar@gmail.com>
2026-06-03 08:40:03 +0000

    Remove dead commented-out `plot_orbit` function

--- visualizer.py
+++ visualizer.py
@@ -2,25 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-# def plot_orbit(bodies):
-#     plt.figure(figsize=(8, 8))
-#     for b in bodies:
-#         positions = np.array(b.positions)
-#         plt.plot(
-#             positions[:, 0],
-#             positions[:, 1],
-#             label = b.name,
-#             color = b.color
-#         )
-
-
-#     plt.scatter(0, 0, s=200, label="Initial Sun Position", color="orange")
-#     plt.xlabel("x position (m)")
-#     plt.ylabel("y position (m)")
-#     plt.axis("equal")
-#     plt.legend()
-#     plt.show()
-
 def animate_orbit(bodies, gen):
     fig, ax = plt.subplots(figsize=(8,8))
     lines = {b: ax.plot([], [], color=b.color, label=b.name, linewidth=1.5)[0] for b in bodies}
