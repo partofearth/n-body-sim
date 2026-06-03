3ea34503f25edca89870d466405b3f40b3c6af0f
PartOfEarth <arnavlotlikar@gmail.com>
2026-06-03 08:57:54 +0000

    Convert visualizer to support 3 dimensions
    - Initilize axes using the 3d subplot projection framework
    - Apply z-axis limits and equal aspect ratios
    - Add `set_3d_properties` to lines and points to track the z-coordinate
    - Disable blitting in animation

--- visualizer.py
+++ visualizer.py
@@ -3,14 +3,18 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 def animate_orbit(bodies, gen):
-    fig, ax = plt.subplots(figsize=(8,8))
+    fig = plt.figure(figsize=(8,8))
+    ax = fig.add_subplot(111, projection="3d")
+
     lines = {b: ax.plot([], [], color=b.color, label=b.name, linewidth=1.5)[0] for b in bodies}
     points = {b: ax.plot([], [], marker='o', color=b.color, markersize=8)[0] for b in bodies    }
     limit = 2.0 * 1.496e11
     ax.set_xlim([-limit, limit])
     ax.set_ylim([-limit, limit])
+    ax.set_zlim([-limit, limit])
     ax.set_xlabel("x position (m)")
     ax.set_ylabel("y position (m)")
+    ax.set_zlabel("z position (m)")
     ax.set_aspect('equal')
     ax.legend(loc="upper left")
 
@@ -21,7 +25,9 @@ def animate_orbit(bodies, gen):
             pos_array = np.array(b.positions)
             if len(pos_array) > 0:
                 lines[b].set_data(pos_array[:, 0], pos_array[:, 1])
+                lines[b].set_3d_properties(pos_array[:, 2])
                 points[b].set_data([pos[0]], [pos[1]])
+                points[b].set_3d_properties([pos[2]])
         return list(lines.values()) + list(points.values())
     
 
@@ -30,7 +36,6 @@ def animate_orbit(bodies, gen):
         update,
         frames=728,
         repeat=False,
-        blit=True,
         interval=10
     )
     plt.show()
